46c5d62f4720a87c62bc783d1baeac3ba71f7b42
Gaogogo <cuggsh@163.com>
2017-10-24 19:29:20 +0800

    add authorize function

--- rpcDemo/client/client.go
+++ rpcDemo/client/client.go
@@ -7,8 +7,8 @@ import (
 )
 
 type User struct {
-	username string
-	passwd   string
+	Username string
+	Passwd   string
 }
 
 func main() {
@@ -18,18 +18,17 @@ func main() {
 		log.Fatal("dialing ERROR :", err)
 	}
 
-	args := User{username: "error", passwd: "error"}
+	args := User{"error", "errors"}
 
 	var gettime time.Time
 	TBegin := time.Now().Local()
 	err = client.Call("GetInfo.GetServerCurrntTime", args, &gettime)
 	duration := time.Since(TBegin)
-	log.Println(gettime.Format("2006-01-02 15:04:05:"), gettime.Nanosecond())
-	gettime = gettime.Truncate(duration / 2)
 
 	if err != nil {
 		log.Fatal("GetInfo error: ", err)
 	}
 
+	gettime = gettime.Truncate(duration / 2)
 	log.Println(gettime.Format("2006-01-02 15:04:05"), gettime.Nanosecond())
 }
